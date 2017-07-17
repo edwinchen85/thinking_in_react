2739ea36b8afd03390e5102be8779c0c1f3142ed
edwinchen85 <edwinchen85@gmail.com>
2017-07-17 17:11:04 +0800

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+}
+
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports = config;
