e16c4ca58182b3a3817a98059c320f7bc03bc7e2
edwinchen85 <edwinchen85@gmail.com>
2017-07-17 17:12:35 +0800

    Add starter <FilterableProductTable />

---
+++ app/components/FilterableProductTable.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class FilterableProductTable extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+module.exports = FilterableProductTable;
